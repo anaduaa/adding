52e905c153ef15ba9611e15e4901656af577bf0b
anaduaa <60792797+anaduaa@users.noreply.github.com>
2020-02-21 14:46:40 +0200

    Update addtwo.java

--- src/add/addtwo.java
+++ src/add/addtwo.java
@@ -3,11 +3,17 @@
 
 public class addtwo {
 
+	public float add( float num1,float num2);
 	   public static void main(String[] args) {
-	        
-	      int num1 = 5, num2 = 15, sum;
-	      sum = num1 + num2;
+	        int s=5;
+		   int d=6;
+	    
+	      sum = add(s,d);
 
 	      System.out.println("Sum of these numbers: "+sum);
 	   }
+	public float add( float num1,float num2){
+		sum = num1 + num2;
+		return sum;
+	}
 	}
